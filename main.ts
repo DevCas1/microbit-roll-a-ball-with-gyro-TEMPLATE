fe58d29616be085006c7ae9777f605a7b9c933b3
Sjouke Yntema <suyntema@live.nl>
2020-09-14 11:43:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ let gyroRoll = 0
 let gyroPitch = 0
 let acceleration = 0
 let deadzone = 0
+let wallLEDBrightness = 0
 basic.forever(function () {
     gyroPitch = input.rotation(Rotation.Pitch)
     gyroRoll = input.rotation(Rotation.Roll)
